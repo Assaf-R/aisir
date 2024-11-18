febf366a7eef4b4aeb18fa400df56b05e26cab40
Assaf-R <Assaf@Reich>
2024-11-19 01:55:33 +0200

    added Logging

--- Readme.md
+++ Readme.md
@@ -1,9 +1,10 @@
 ### Aisir
-Named after the beautiful loch Aisir in northen Scotland, Aisir is an Ebpf based tool that logs and filters connection to remote IP addresses
+Named after the beautiful loch Aisir in northen Scotland, Aisir is an eBpf based tool that logs and filters connection to remote IP addresses
 
 ### How does this work?
 The python program - aisir.py - loads the bpf c program - aisir_bpf.c - and hooks the connect syscall with a kprobe.
-The ebpf program checks if the remote IP is in the ip_list.txt file.
+The eBpf program checks if the remote IP is in the ip_list.txt file.
+The results are logged to /var/log/loch/aisirX.log
 
 ***notes***
 - The ip_list.txt needs to be in the same dir with the programs
--- aisir.py
+++ aisir.py
@@ -1,16 +1,20 @@
 
 # aisir.py
-# Ebpf based local firewall tool by Assaf R.
+# eBpf based local firewall tool by Assaf R.
 # --- --- ---
 
 # Imports
 from bcc import BPF
-import socket, struct, ctypes
+import socket, struct, ctypes, os, time
 
 # Constants
 EBPF_PROGRAM = "aisir_bpf.c"
 IP_BLACKLIST = "ip_list.txt"
 BUFFER_SIZE = 128 * 1024  # 128KB
+LOG_FOLDER = "/var/log/loch"
+LOG_NAME = "aisir"
+LOG_EXTENT = ".log"
+LOG_MAX_SIZE = 8192
 
 # Load C program
 with open (EBPF_PROGRAM, 'r') as raw_program:
@@ -28,6 +32,38 @@ def load_blacklist():
             b['ip_blacklist'][ctypes.c_uint(struct.unpack('I', socket.inet_aton(ip))[0])] = ctypes.c_int(1) # The 1 is for the c part
    
 
+def logging(event):
+    '''
+    The log files called aisirX.log where X are incrementing numbers so every log file isn't too big.
+    If the folder doesn't exist it creates it
+    '''
+    try:
+        os.mkdir(LOG_FOLDER)
+    except FileExistsError:
+        pass
+
+    log_number = 1
+    last_log = False
+    while not last_log:
+        if os.path.isfile(f"{LOG_FOLDER}/{LOG_NAME}{log_number}{LOG_EXTENT}"):
+            file_size = os.stat(f"{LOG_FOLDER}/{LOG_NAME}{log_number}{LOG_EXTENT}").st_size
+            if file_size > LOG_MAX_SIZE:
+                log_number += 1
+            else:
+                last_log = True
+        else:
+            last_log = True
+    with open(f"{LOG_FOLDER}/{LOG_NAME}{log_number}{LOG_EXTENT}", 'a') as log_file:
+        log_file.write(f"{event}\n") 
+
+def format_events(event):
+
+    msg = ""
+    for key in event:
+        msg += f"{key} - {event[key]}, "
+
+    print(msg)
+
 def process_data(cpu, data, size):
     '''
     converts data to dictionary and print it
@@ -41,10 +77,10 @@ def process_data(cpu, data, size):
         "process_name": data.process_name.decode(),
         "parent_process_name": data.parent_process_name.decode(),
         "target_ip":socket.inet_ntoa(struct.pack('I', data.target_ip)),
-        "res":data.res
+        "blocked":data.blocked
     }
-    
-    print(event)
+
+    logging(format_events(event))
 
 def main():
     print("starting")
